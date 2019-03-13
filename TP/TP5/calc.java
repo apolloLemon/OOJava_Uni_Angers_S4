a294a77162fbe6d8c511034834c0a4d91841e28d
Matthew Coyle <matthew@coyle.fr>
2019-03-13 16:39:26 +0100

    calc v1

--- TP/TP5/calc.java
+++ TP/TP5/calc.java
@@ -1,18 +1,99 @@
-import java.util.Scanner;
+import java.util.*;
 
 class Calculatrice {
 
-	double saisieNombre(){
+	//Calculatrice(){}
+
+	double saisieNombre() throws InputMismatchException {
 		System.out.println("Saisie un Double: ");
 		Scanner sc = new Scanner(System.in);
 		double a = sc.nextDouble();
-		sc.close();
+		//sc.close();
 		return a;
 	}
 
-	double lancementCalcul(){
-		double a = saisieNombre();
+	class InvalidOpExecption extends Exception {}
+
+	String saisieOperation() throws InvalidOpExecption{
+		System.out.println("Saisie une Operation: ");
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		//sc.close();
+
+		switch(a) {
+			case "+":
+			case "*":
+			case "-":
+			case "/":
+			case "%":
+				return a;
+			default:
+				throw new InvalidOpExecption();
+		}
 	}
 
-	
+	double lancementCalcul(){
+		double a=0, b=0;
+		String op="";
+		Boolean flag;
+		do {
+			flag = false;
+			try {
+				a = saisieNombre();
+			} catch (InputMismatchException e) {
+				System.out.println("Erreur de saisi");
+				flag = true;
+			}
+
+		} while(flag);
+
+		do {
+			flag = false;
+			try {
+				op = saisieOperation();
+			} catch (InvalidOpExecption e) {
+				System.out.println("Erreur de saisi");
+				flag = true;
+			}
+
+		} while(flag);
+
+		do {
+			flag = false;
+			try {
+				b = saisieNombre();
+			} catch (InputMismatchException e) {
+				System.out.println("Erreur de saisi");
+				flag = true;
+			}
+
+		} while(flag);
+
+		switch(op){
+			case "+":
+				return a+b;
+			case "-":
+				return a-b;
+			case "*":
+				return a*b;
+			case "/":
+				return a/b;
+			case "%":
+				return a%b;
+			default:
+				return a;
+		}
+
+		//return a;
+	}
+
+
+}
+
+
+class TestOne {
+	public static void main (String args[]){
+		Calculatrice c = new Calculatrice();
+		System.out.println("Le resultat est "+c.lancementCalcul());
+	}
 }
